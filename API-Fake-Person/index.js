20debcce846d1c39e9c9991f8a1c1eeea00f0512
José Robério <jose.silva2399@etec.sp.gov.br>
2025-09-17 21:54:45 -0300

    Add files via upload

---
+++ API-Fake-Person/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const gerarpessoa = require('./utils/gerarpessoa');
+
+const app = express();
+const port = 3000;
+
+app.get('/pessoa', (req,res) => {
+  const qtd = parseInt(req.query.qtd) || 1;
+  const formato = req.query.formato || "json";
+
+    const lista = []
+    for(let i = 1; i <= qtd; i++) {
+        lista.push(gerarpessoa());
+    }
+
+    if (formato ==='sql') {
+      const sql = lista.map( p => {
+        return `INSERT INTO pessoas (nome, cpf, nome,pai, nome_mae, data_nascimento, idade, endereco, bairro, cidade, estado, cep, naturalidade) VALUES ('${p.nomecompleto}', '${p.cpf}', '${p.nomepai}','${p.nomemae}', '${p.data_nascimento}','${p.idade}','${p.enderecofinal}','${p.bairro}','${p.cidade}','${p.estado}','${p.cep}','${p.naturalidade}');`;
+      }).join('\n');
+
+      res.type('text/plain').send(sql);
+    } else {
+      res.json(lista);
+    }
+});
+
+app.listen(port, () => {
+  console.log(`Rodando em http://localhost:${port}`);
+});
---
+++ API-Fake-Person/utils/gerarpessoa.js
@@ -0,0 +1,51 @@
+const nomes = require('../data/nomes.json');
+const nomesmae = require('../data/nomesmae.json');
+const nomespai = require('../data/nomespai.json');
+const enderecos = require('../data/enderecos.json');
+const nacionalidade = require('../data/nacionalidade.json');
+const sobrenomes = require('../data/sobrenomes.json');
+const cpf = require('gerar-cpf');
+const { faker } = require('@faker-js/faker');
+
+function gerardtnascimento(idade){
+    const ano = new Date().getFullYear() - idade;
+    const mes = String(Math.floor(Math.random() * 12) + 1).padStart(2, '0')
+    const dia = String(Math.floor(Math.random() * 28) + 1).padStart(2, '0')
+
+    return `${dia}/${mes}/${ano}`;
+}
+
+
+function gerarpessoa() {
+const nome = nomes[Math.floor(Math.random() * nomes.length)];
+const sobrenome = sobrenomes[Math.floor(Math.random() * sobrenomes.length)];
+const nomepai = nomespai[Math.floor(Math.random() * nomespai.length)];
+const nomemae = nomesmae[Math.floor(Math.random() * nomesmae.length)];
+const idade = Math.floor(Math.random() * 60) +18;
+const endereco = enderecos.enderecos[Math.floor(Math.random() * enderecos.enderecos.length)];
+const bairro = endereco.bairro;
+const cidade = endereco.cidade;
+const estado = endereco.estado;
+const cep = endereco.cep;
+const naturalidade = endereco.naturalidade;
+const enderecofinal = `${endereco.rua}, ${endereco.numero}`;
+
+
+const tel = faker.phone.number('(DD) 9XXXX-XXXX');
+return {
+    nomecompleto: `${nome} ${sobrenome}`,
+    cpf: cpf(),
+    nomepai: `${nomepai} ${sobrenome}`,
+    nomemae: `${nomemae} ${sobrenome}`,
+    data_nascimento: gerardtnascimento(idade),
+    idade,
+    enderecofinal,
+    bairro,
+    cidade,
+    estado,
+    cep,
+    naturalidade,   
+};
+}
+
+module.exports = gerarpessoa;
