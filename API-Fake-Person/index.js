b1ac2f5a4dd7ad3687a601d9b61cfc7790246769
José Robério <jose.silva2399@etec.sp.gov.br>
2025-09-18 12:47:26 -0300

    Update index.js

--- API-Fake-Person/index.js
+++ API-Fake-Person/index.js
@@ -14,10 +14,11 @@ app.get('/pessoa', (req,res) => {
     }
 
     if (formato ==='sql') {
-      const sql = lista.map( p => {
-        return `('${p.nomecompleto}', '${p.cpf}', '${p.nomepai}','${p.nomemae}', '${p.data_nascimento}','${p.idade}','${p.enderecofinal}','${p.bairro}','${p.cidade}','${p.estado}','${p.cep}','${p.naturalidade}');`;
+      const values = lista.map( p => {
+        return `('${p.nomecompleto}', '${p.cpf}', '${p.nomepai}','${p.nomemae}', '${p.data_nascimento}','${p.idade}','${p.enderecofinal}','${p.bairro}','${p.cidade}','${p.estado}','${p.cep}','${p.naturalidade}')`;
       }).join(',\n');
 
+      const sql = `INSERT INTO pessoas (nome, cpf, nm_pai, nm_mae, dt_nascimento, idade, endereco, bairro, cidade, estado, cep, naturalidade) VALUES\n${values};`;
       res.type('text/plain').send(sql);
     } else {
       res.json(lista);
@@ -28,4 +29,5 @@ app.listen(port, () => {
   console.log(`Rodando em http://localhost:${port}`);
 
 });
+
 
